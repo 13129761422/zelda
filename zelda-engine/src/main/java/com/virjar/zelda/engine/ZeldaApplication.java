c07bf2f2c209f97a10ec7ef7a08b43adc02f88d4
virjar <virjar@virjar.com>
2020-02-16 23:21:59 +0800

    换一种解决IO重定向副作用的方法

--- zelda-engine/src/main/java/com/virjar/zelda/engine/ZeldaApplication.java
+++ zelda-engine/src/main/java/com/virjar/zelda/engine/ZeldaApplication.java
@@ -4,8 +4,17 @@ import android.app.Application;
 import android.content.Context;
 
 public class ZeldaApplication extends Application {
+    private static boolean callAttachBaseContext = false;
+    private static boolean callApplicationOnCreate = false;
+
     @Override
     protected void attachBaseContext(Context base) {
+        if (callAttachBaseContext) {
+            return;
+        }
+        //the ZeldaApplication will be create again after apk sourceDir IO relocation,
+        //and we only call ZeldaRuntime.callApplicationAttach one times
+        callAttachBaseContext = true;
         try {
             ZeldaRuntime.callApplicationAttach(base);
         } catch (Throwable throwable) {
@@ -17,6 +26,10 @@ public class ZeldaApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (callApplicationOnCreate) {
+            return;
+        }
+        callApplicationOnCreate = true;
         ZeldaRuntime.callApplicationOnCreate();
     }
 }
--- zelda-engine/src/main/java/com/virjar/zelda/engine/fixer/AssertFixer.java
+++ zelda-engine/src/main/java/com/virjar/zelda/engine/fixer/AssertFixer.java
@@ -1,20 +1,11 @@
 package com.virjar.zelda.engine.fixer;
 
-import android.content.res.AssetManager;
-import android.content.res.Resources;
-import android.util.Log;
-
-import com.virjar.zelda.buildsrc.Constants;
 import com.virjar.zelda.engine.NativeEngine;
 import com.virjar.zelda.engine.ZeldaEnvironment;
 import com.virjar.zelda.engine.ZeldaRuntime;
 
 import java.io.File;
 import java.io.IOException;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
-import mirror.android.app.ActivityThread;
 
 /**
  * 修复媒资管理器相关，涉及 ResourceManager 和 AssertManager
@@ -31,25 +22,6 @@ public class AssertFixer {
         // /data/app/zelda.comtentcentmm/base.apk -> /data/data/zelda.comtentcentmm/xxx/file/zelda_resource/origin_apk.apk
         NativeEngine.redirectFile(sourceCanonicalPath, destCanonicalPath);
         NativeEngine.redirectFile(sourceCanonicalPath.replace(ZeldaRuntime.nowPackageName, ZeldaRuntime.originPackageName), destCanonicalPath);
-
-        Object mBoundApplication = ActivityThread.mBoundApplication.get(ZeldaRuntime.mainThread);
-        if (mBoundApplication != null) {
-            Object loadApk = XposedHelpers.getObjectField(mBoundApplication, "info");
-            Resources resources = (Resources) XposedHelpers.getObjectField(loadApk, "mResources");
-            final AssetManager assetManager = resources.getAssets();
-
-            XposedHelpers.findAndHookMethod(AssetManager.class, "isUpToDate", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    if (param.thisObject.equals(assetManager)) {
-                        //prevent create new loadApk | application
-                        param.setResult(true);
-                    }
-                }
-            });
-        } else {
-            Log.w(Constants.TAG, "can not find mBoundApplication from ActivityThread!!");
-        }
     }
 
 }
